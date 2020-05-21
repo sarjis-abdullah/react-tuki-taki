5048e5339325147cef0d8a4b86d08e283451b57e
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-22 00:34:39 +0600

    nav bar created newly

---
+++ src/landingPage/SignInLinks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+const SignInLinks = () => {
+    return (
+        <div>
+            <Button color="inherit">New Project</Button>
+            <Button color="inherit">Sign out</Button>
+        </div>
+    )
+}
+
+export default SignInLinks
---
+++ src/landingPage/SignOutLinks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+const SignOutLinks = () => {
+    return (
+        <div>
+            <Button color="inherit">Sign up</Button>
+            <Button color="inherit">Login</Button>
+        </div>
+    )
+}
+
+export default SignOutLinks

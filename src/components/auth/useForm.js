5231e18ff6e465b0804c1f1458847b54e9fb9844
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-22 19:57:39 +0600

    aligned

--- src/components/auth/useForm.js
+++ src/components/auth/useForm.js
@@ -1,32 +1,33 @@
 import React from 'react'
 
 const useForm = (submit, validate) => {
-    let formData = { name: '', email: '' }
-    const [form, setForm] = React.useState(formData);
-    const [errors, setErrors] = React.useState({});
-    const [isSubmitting, setIsSubmitting] = React.useState(false);
-    const handleChanges = (e) => {
-        const { name, value } = e.target
-        setForm({
-            ...form,
-            [name]: value
-        })
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setErrors(validate(form))
-        setIsSubmitting(true)
-    }
-    React.useEffect(()=> {
-        if (!Object.keys(errors).length && isSubmitting) {
-            submit()
-        }
-    }, [errors])
-    return {
-        handleSubmit,
-        handleChanges,
-        form,
-        errors
-    }
+	let formData = { name: '', email: '' }
+	const [form, setForm] = React.useState(formData);
+	const [errors, setErrors] = React.useState({});
+	const [isSubmitting, setIsSubmitting] = React.useState(false);
+	const handleChanges = (e) => {
+		const { name, value } = e.target
+		setForm({
+			...form,
+			[name]: value
+		})
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		setErrors(validate(form))
+		setIsSubmitting(true)
+	}
+	React.useEffect(() => {
+		if (!Object.keys(errors).length && isSubmitting) {
+			submit()
+		}
+	}, [errors])
+
+	return {
+		handleSubmit,
+		handleChanges,
+		form,
+		errors
+	}
 }
 export default useForm

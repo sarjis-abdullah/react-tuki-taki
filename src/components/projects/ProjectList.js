e1bdcef15544aaafff814a5d8a45d36d874dfacb
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-22 11:20:23 +0600

    summary page made

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import ProjectSummary from './ProjectSummary'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-    media: {
-        height: 140,
-    },
-}));
 
 export default function CenteredGrid() {
-    const classes = useStyles();
     const todo = [
         { title: 'Test Title' },
         { title: 'Test Title' },
@@ -31,40 +10,11 @@ export default function CenteredGrid() {
         { title: 'Test Title' },
     ]
     return (
-        <div className={classes.root}>
-            {
-                todo.map((item, index) => {
-                    return (
-                        <Card key={index} className={classes.root}>
-                            <CardActionArea>
-                                <CardMedia
-                                    className={classes.media}
-                                    image="https://via.placeholder.com/600/24f355"
-                                    title="Contemplative Reptile"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {item.title}
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                        across all continents except Antarctica
-            </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Share
-            </Button>
-                                <Button size="small" color="primary">
-                                    Learn More
-            </Button>
-                            </CardActions>
-                        </Card>
-
-                    )
-                })
-            }
+        <div>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div >
     );
 }

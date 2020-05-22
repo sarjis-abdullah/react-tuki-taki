80afcd58304e396bfd1c2ba4b871e9d7ecf31900
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-22 19:44:46 +0600

    form validated

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -3,18 +3,31 @@ import { FormControl, InputLabel, OutlinedInput, Grid, Button } from '@material-
 import useForm from './UseForm'
 
 export default function StateTextFields(props) {
-    const {form, handleChanges, handleSubmit} = useForm(submit)
-    function submit() {
-        
-        let isValid = form.name && form.name.length && form.email && form.email.length
-        if (isValid) {
-            console.log(form)
-            document.getElementById("loginForm").reset();
-            // e.target.reset()
+    const validate = (values) => {
+        let { name, email } = values
+        let errors = {}
+        if (!email) {
+            errors.email = 'Email is required'
         } else {
-            alert('nop')
+            let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            let valid = re.test(email.toLowerCase());
+            if (!valid) errors.email = 'Email is invalid';
         }
+        if (!name) {
+            errors.name = 'Name is required'
+        } else if (name.length < 3) {
+            errors.name = 'Minimum 4 letter'
+        }
+        return errors
+    }
+    const { form, handleChanges, handleSubmit, errors } = useForm(submit, validate)
+    function submit() {
+        console.log(form)
+           // document.getElementById("loginForm").reset();
+            // e.target.reset()
+        
     };
+
     return (
         <Grid container spacing={3} style={{ justifyContent: 'center', marginTop: 10 }}>
             <Grid item xs={8}>
@@ -29,6 +42,7 @@ export default function StateTextFields(props) {
                             onChange={handleChanges}
                             labelWidth={60}
                         />
+                        {errors.name && <p>{errors.name}</p>}
                     </FormControl>
                     <FormControl fullWidth variant="outlined" style={{ marginTop: 5 }}>
                         <InputLabel htmlFor="outlined-adornment-amount">
@@ -40,6 +54,7 @@ export default function StateTextFields(props) {
                             onChange={handleChanges}
                             labelWidth={60}
                         />
+                        {errors.email && <p>{errors.email}</p>}
                     </FormControl>
                     <Button type="submit" variant="contained" color="primary">
                         Login
--- src/components/auth/useForm.js
+++ src/components/auth/useForm.js
@@ -1,22 +1,32 @@
 import React from 'react'
 
- const useForm = (submit) => {
-    const [form, setForm] = React.useState({ name: '', email: '' });
+const useForm = (submit, validate) => {
+    let formData = { name: '', email: '' }
+    const [form, setForm] = React.useState(formData);
+    const [errors, setErrors] = React.useState({});
+    const [isSubmitting, setIsSubmitting] = React.useState(false);
     const handleChanges = (e) => {
-        const {name, value} = e.target
+        const { name, value } = e.target
         setForm({
             ...form,
             [name]: value
         })
     }
-    const handleSubmit = (e)=> {
+    const handleSubmit = (e) => {
         e.preventDefault()
-        submit()
+        setErrors(validate(form))
+        setIsSubmitting(true)
     }
+    React.useEffect(()=> {
+        if (!Object.keys(errors).length && isSubmitting) {
+            submit()
+        }
+    }, [errors])
     return {
         handleSubmit,
         handleChanges,
-        form
+        form,
+        errors
     }
 }
 export default useForm

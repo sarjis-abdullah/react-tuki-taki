853a37f3af887b14f1a1808e8552feabe8cf1760
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-23 16:49:24 +0600

    e

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,29 +1,35 @@
 const initialState = {
-    projects: [
-        {
-            "postId": 1,
-            "id": 1,
-            "name": "id labore ex et quam laborum",
-            "email": "Eliseo@gardner.biz",
-            "title": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-          },
-          {
-            "postId": 1,
-            "id": 2,
-            "name": "quo vero reiciendis velit similique earum",
-            "email": "Jayne_Kuhic@sydney.com",
-            "title": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-          },
-          {
-            "postId": 1,
-            "id": 3,
-            "name": "odio adipisci rerum aut animi",
-            "email": "Nikita@garfield.biz",
-            "title": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
-          },
-    ]
+  projects: [
+    {
+      "postId": 1,
+      "id": 1,
+      "name": "id labore ex et quam laborum",
+      "email": "Eliseo@gardner.biz",
+      "title": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+      "postId": 1,
+      "id": 2,
+      "name": "quo vero reiciendis velit similique earum",
+      "email": "Jayne_Kuhic@sydney.com",
+      "title": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+    },
+    {
+      "postId": 1,
+      "id": 3,
+      "name": "odio adipisci rerum aut animi",
+      "email": "Nikita@garfield.biz",
+      "title": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+    },
+  ]
 }
 const projectReducer = (state = initialState, action) => {
-    return state
+  switch (action.type) {
+    case 'createProject':
+      console.log('object', action)
+      return action
+    default:
+      return state
+  }
 }
 export default projectReducer

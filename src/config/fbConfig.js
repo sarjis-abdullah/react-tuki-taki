f8eebe9d99ee675e8bab2866a5a89c6ed770be92
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-23 20:28:32 +0600

    firebase added

---
+++ src/config/fbConfig.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyCs4ori0cwS_pMm9LKSFZjaV35-yH8oUSY",
+    authDomain: "react-tuki-taki.firebaseapp.com",
+    databaseURL: "https://react-tuki-taki.firebaseio.com",
+    projectId: "react-tuki-taki",
+    storageBucket: "react-tuki-taki.appspot.com",
+    messagingSenderId: "558418454982",
+    appId: "1:558418454982:web:1729cdcff60d5010cdaac7",
+    measurementId: "G-51G0050CDM"
+  };

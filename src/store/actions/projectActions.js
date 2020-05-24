af9919e7933805e0cdfa3f0313a5bafdb760a92a
sarjis-abdullah <sarjis@reformedtech.org>
2020-05-24 12:08:35 +0600

    q

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,29 +1,27 @@
-import { firestore } from '../../config/fbConfig'
+//import { firestore } from '../../config/fbConfig'
 
 
 export const createProject = (project) => {
-	let { type, dispatch, payload } = project
-	const data = {
-		id: Date.now(),
-		name: 'Sarjis',
-		email: '',
-		title: payload.name
-	}
-  //dispatch({ type, data })
-  
-//	return (dispatch, getState, {getFirebase, getFirestore}) => {
-    // make async call to database
-    //const firestore = getFirestore();
+  let { type, payload } = project
+  const data = {
+    id: Date.now(),
+    name: 'Sarjis',
+    email: '',
+    title: payload.name
+  }
+
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    console.log(getState)
+    const firestore = getFirestore();
     firestore.collection('projects').add({
       title: payload.name,
       body: payload.name,
       firstName: 'Net',
       lastName: 'Ninja'
     }).then((res) => {
-      console.log(res)
       dispatch({ type, data })
     }).catch(err => {
       dispatch({ type: 'CREATE_PROJECT_ERROR' }, err);
     });
- // }
+  }
 }
